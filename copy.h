9b6dad74377bef4cdac627be297fc41fc0042d35
Rishitha Minol <rishithaminol@gmail.com>
2016-01-11 22:39:05 +0530

    added COPY_H and IN_LIST_H macros to prevent accidental double-inclusion.

--- copy.h
+++ copy.h
@@ -1,2 +1,7 @@
+#ifndef COPY_H
+#define COPY_H
+
 extern void copy(const char *src, const char *dest);
 extern void remove_tmpdir(const char *f);
+
+#endif /* COPY_H */
--- in_list.h
+++ in_list.h
@@ -1,4 +1,6 @@
 /* all the features in this file is used by copy.c:copy_hlink() */
+#ifndef IN_LIST_H
+#define IN_LIST_H
 
 /* ATTENTION! 
  * all the functions are compatible only with this structure
@@ -9,3 +11,5 @@ struct in_list *in_new_segment(int old_inode, const char *y);
 void in_append(int old_inode, const char *y, struct in_list *z);
 char *in_search(int old_inode, struct in_list *z);
 void in_free(struct in_list *z);
+
+#endif
