d4bdb7106c21441ffea0a844d4b7d904be1f8121
Rishitha Minol <rishithaminol@gmail.com>
2016-01-24 22:02:24 +0530

    Added MPKG_DB_ macro which shows the path of the mpkg's database.

--- mpkg.h
+++ mpkg.h
@@ -1,6 +1,10 @@
 #ifndef MPKG_H
 #define MPKG_H
 
+#ifndef MPKG_DB_
+#define MPKG_DB_ "/var/lib/mpkg/mpkg.db"
+#endif
+
 extern char *tmp_dir;
 extern char *tmp_data_dir;
 extern char *tmp_config_dir;
