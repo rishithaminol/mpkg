a169a1e7ff96f99658843da3ccd6dc7fb79ac154
Rishitha Minol <rishithaminol@gmail.com>
2016-01-28 21:26:52 +0530

    quotation mark

--- scripts/del_executables.sh
+++ scripts/del_executables.sh
@@ -7,7 +7,7 @@ fi
 
 find "$1" -type f ! -path "*/json-c/*" ! -path "*/tests/root*" -perm /u=x,g=x,o=x | \
 while read file_name; do
-	string="$(file "$file_name" | grep 'ASCII text')"
+	string=$(file "$file_name" | grep 'ASCII text')
 
 	if [ -z "$string" ]; then
 		rm -v "$file_name"
