45799cba6e782df3d8f50eb58ede81d6a1441cad
Rishitha Minol <rishithaminol@gmail.com>
2016-03-19 16:33:40 +0530

    modified:   mpkg.c

--- mpkg.c
+++ mpkg.c
@@ -6,15 +6,19 @@
 #include <linux/limits.h>	/* NAME_MAX */
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>			/* open() */
+#include <sqlite3.h>
+#include <unistd.h>
 
 #include "mpkg.h"
 #include "utils.h"
 #include "copy.h"
 #include "ar.h"
 #include "info.h"
+#include "sqlfrms.h"
 #include "version.h"
 
-char temp_str[4096];	/* keep temporary strings */
+char temp_str[4096];
 
 char prefix[4096];		/*!< @brief Prefix path (destination installation) */
 char *prog_name = NULL;	/*!< @brief Program name. */
@@ -53,11 +57,21 @@ static void _get_info(char *str, int n)
 	count++;
 }
 
+/*! @brief used by copy() */
+static void copy_log(void *fd, char *fpath)
+{
+	char t[256];
+	sprintf(t, "%s\n", fpath);
+
+	write(*((int *)fd), t, strlen(t) + 1);
+}
+
 int main(int argc, char *argv[])
 {
 	int ret, c;
 	ar_object *ar1;
 	info_object *info;
+	sqlite3 *db = NULL;
 
 	prog_name = *argv;
 
@@ -128,33 +142,42 @@ int main(int argc, char *argv[])
 	/* Create environment */
 	prepare_tempds();
 
-	/* if archive open fail 'goto clean_out' */
+	/* if archive open fail 'goto wind_up' */
 	ar1 = ar_open(archive);
 	if (ar1 == NULL)
-		goto clean_out;
+		goto wind_up;
 	ar_grab(ar1, "info", _get_info);
 	info = info_load(temp_str);
 
 	if (infoflag > 0) {	/* if '--info' used */
 		printf("info details\n");
 		info_print(info, argv[infoflag]);
-		goto clean_out;
+		goto wind_up;
 	}
 
 	ar_extract_all(ar1, TMP_DIR);	/* extract all files to TMP_DIR */
-	tar_extract("/tmp/mpkg/control.tar.gz", TMP_CONFIG_DIR);
-	tar_extract("/tmp/mpkg/data.tar.gz", TMP_DATA_DIR);
-
+	tar_extract(path_append(TMP_DIR, "control.tar.gz"), TMP_CONFIG_DIR);
+	tar_extract(path_append(TMP_DIR, "data.tar.gz"), TMP_DATA_DIR);
 	/* there should be control directory handlers */
 	/* after extraction there sould be pre install function handle */
 
-	copy(TMP_DATA_DIR, prefix, NULL, NULL);
+/* file copying to the prefix */
+	/* copy log */
+	int fd = open(path_append(".", (info_get_fld(info, fld_pkg))->str), O_WRONLY|O_CREAT|O_EXCL,
+		S_IRUSR|S_IWUSR);
+	copy(TMP_DATA_DIR, prefix, (void *)&fd, copy_log);
+
+/* Database update section */
+	db = open_main_db();
+	update_db(db, info, path_append(".", (info_get_fld(info, fld_pkg))->str));
 
 	/* after copying there sould be post installation function handle */
-clean_out:
+wind_up:
 	ar_close(ar1);
 	info_unload(info);
+	db ? close_main_db(db) : FALSE;
 	clean_temps ? remove_tmpdir(TMP_DIR) : FALSE;
+	fd ? close(fd) : FALSE;
 
 	return 0;
 }
@@ -163,8 +186,7 @@ static int tar_extract(const char *src, const char *dest)
 {
 	char cmd[NAME_MAX];
 
-	sprintf(cmd, "/bin/tar -xf %s", src);
-	sprintf(cmd, "%s -C %s", cmd, dest);
+	sprintf(cmd, "/bin/tar -xf %s -C %s", src, dest);
 
 	if (system(cmd) != 0) {
 		printf("%s: error occured while extracting '%s'\n", prog_name, src);
