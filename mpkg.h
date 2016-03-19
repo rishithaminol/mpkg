ade01a269e18a0fd39551961a0ce2c4b48914de3
Rishitha Minol <rishithaminol@gmail.com>
2016-03-19 15:19:12 +0530

    Changed TMP_DATA_DIR and TMP_CONFIG_DIR macro data

--- mpkg.h
+++ mpkg.h
@@ -5,10 +5,12 @@
 #define MPKG_DB_ "/var/lib/mpkg/mpkg.db"
 #endif
 
+#include "utils.h"
+
 #ifndef CUSTOM_TEMP_LOCATION
 #define TMP_DIR "/tmp/mpkg"
-#define TMP_DATA_DIR "/tmp/mpkg/data"
-#define TMP_CONFIG_DIR "/tmp/mpkg/config"
+#define TMP_DATA_DIR path_append(TMP_DIR, "data")
+#define TMP_CONFIG_DIR path_append(TMP_DIR, "config")
 #endif
 
 extern char prefix[4096];
