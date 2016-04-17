166cb94bb5dd70b42b851de8e417af7bb4bae25b
Rishitha Minol <rishithaminol@gmail.com>
2016-04-17 15:56:16 +0530

    Removed json-c path

--- scripts/del_executables.sh
+++ scripts/del_executables.sh
@@ -5,7 +5,7 @@ if (( $# < 1 )); then
 	exit 1
 fi
 
-find "$1" -type f ! -path "*/json-c/*" ! -path "*/tests/root*" -perm /u=x,g=x,o=x | \
+find "$1" -type f ! -path "*/tests/root*" -perm /u=x,g=x,o=x | \
 while read file_name; do
 	string=$(file "$file_name" | grep 'ASCII text')
 
