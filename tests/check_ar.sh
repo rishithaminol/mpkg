8cf8fdf9ef123b95e7580313573a0535250eb8a0
Rishitha Minol <rishithaminol@gmail.com>
2016-04-18 11:30:27 +0530

    Changed ar checking methods

--- tests/check_ar.sh
+++ tests/check_ar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # compile
-gcc -Wall -g -DDEBUG___ ../ar.c ar.c -o ar_test
+gcc -Wall -g -DDEBUG___ -c ../mpkg_err.c
+gcc -Wall -g -DDEBUG___ -c ../ar.c
+gcc -Wall -g -c ar_test.c
+gcc -Wall -g mpkg_err.o ar.o ar_test.o -o ./ar_test
 
-./ar_test files/ncurses-5.9.mav
+./ar_test files/ncurses-5.9-1.0.1.amd64.mav
